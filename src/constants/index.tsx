77f3982e14b37e589734de7e889c372fd939bee9
Victor <victorm.martinezjr@outlook.com>
2026-05-18 16:19:14 -0400

    chore(constants): add  Navbar links

---
+++ src/constants/index.tsx
@@ -0,0 +1,12 @@
+interface Link {
+  text: string;
+  href: string;
+}
+export const LINKS: Link[] = [
+  { text: "Dishes", href: "#dishes" },
+  { text: "About", href: "#about" },
+  { text: "Mission", href: "#mission" },
+  { text: "Expertise", href: "#expertise" },
+  { text: "Review", href: "#review" },
+  { text: "Contact", href: "#contact" },
+];
