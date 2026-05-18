f32d1d6bca0598cfb6ec37ba5dd36bc454e83701
Victor <victorm.martinezjr@outlook.com>
2026-05-18 19:54:53 -0400

    chore(constants): add mission data

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -94,3 +94,12 @@ export const ABOUT: AboutContent = {
   content:
     "At Restaura, we believe that great food goes beyond taste; it tells a story of dedication and creativity. From our chef's signature creations to our attentive service, every detail is curated to ensure your visit is nothing short of exceptional. Whether you're savoring our renowned Tikka Kebab or exploring our diverse menu inspired by global flavors, each dish is a celebration of flavor and innovation. Join us for a culinary journey where every bite leaves a lasting impression. Experience Restaura—where every meal is a masterpiece.",
 };
+
+// Mission statement
+interface MissionContent {
+  statement: string;
+}
+export const MISSION: MissionContent = {
+  statement:
+    "At our restaurant, our mission is to create delicious and memorable dining experiences.",
+};
