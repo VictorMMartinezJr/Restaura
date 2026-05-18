074f99327e3f3878c44a913b9a8d5f38db01e716
Victor <victorm.martinezjr@outlook.com>
2026-05-18 19:56:28 -0400

    feat(ui): create Mission section

---
+++ src/components/Mission.tsx
@@ -0,0 +1,34 @@
+import { MISSION } from "../constants";
+import mission from "../assets/mission.jpeg";
+
+const Mission = () => {
+  return (
+    <section id="mission">
+      <div className="container mx-auto text-center">
+        {/* Title */}
+        <h2 className="mb-8 text-3xl lg:text-4xl">Our Mission</h2>
+
+        {/* Video */}
+        <div className="relative flex items-center justify-center">
+          <video
+            src="https://res.cloudinary.com/dosqnvzhy/video/upload/v1779148377/mission_rnfzst.mp4"
+            className="w-full rounded-3xl pointer-events-none"
+            autoPlay
+            muted
+            loop
+            playsInline
+            poster={mission}
+          ></video>
+
+          {/* Overlay and text */}
+          <div className="absolute h-full w-full rounded-3xl bg-black/40"></div>
+          <p className="absolute max-w-lg tracking-tighter lg:text-3xl">
+            {MISSION.statement}
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Mission;
