c0c238fdf32bd59d16cd4add49590f0bad7c536a
Victor <victorm.martinezjr@outlook.com>
2026-05-18 21:54:22 -0400

    feat(ui): add animation to Expertise section

--- src/components/Enterprise.tsx
+++ src/components/Enterprise.tsx
@@ -1,16 +1,37 @@
 import { CUSINES } from "../constants";
+import { motion } from "framer-motion";
+
+const containerVariants = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 1,
+    },
+  },
+};
+
+const itemVariants = {
+  hidden: { opacity: 0, y: 20 },
+  show: { opacity: 1, y: 0, transition: { duration: 0.8 } },
+};
 
 const Enterprise = () => {
   return (
     <section id="expertise">
-        {/* Title */}
+      {/* Title */}
       <h2 className="my-8 text-center text-3xl tracking-tighter lg:text-4xl">
         Our Expertise
       </h2>
 
-      <div className="container mx-auto px-4">
+      <motion.div
+        initial="hidden"
+        whileInView="show"
+        variants={containerVariants}
+      >
         {CUSINES.map((cuisine, index) => (
-          <div
+          <motion.div
+            variants={itemVariants}
             key={index}
             className="flex items-center border-b-4 border-dotted border-neutral-700/40 py-2"
           >
@@ -31,11 +52,13 @@ const Enterprise = () => {
               <h3 className="text-2xl uppercase tracking-tighter text-rose-300">
                 {cuisine.title}
               </h3>
-              <p className="mt-4 text-lg tracking-tighter">{cuisine.description}</p>
+              <p className="mt-4 text-lg tracking-tighter">
+                {cuisine.description}
+              </p>
             </div>
-          </div>
+          </motion.div>
         ))}
-      </div>
+      </motion.div>
     </section>
   );
 };
