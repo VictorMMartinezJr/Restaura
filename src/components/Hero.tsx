d854ad46a5d481a5024d190392af046553aa176b
Victor <victorm.martinezjr@outlook.com>
2026-05-18 21:40:17 -0400

    feat(ui): add framer motion and add animation to hero logo

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,11 @@
+import { motion } from "framer-motion";
 import heroIMG from "../assets//hero/hero.jpeg";
 import logo from "../assets/hero/logo.png";
 
 const Hero = () => {
   return (
     <section className="relative flex h-screen items-center justify-center">
-        {/* Background Video */}
+      {/* Background Video */}
       <div className="absolute inset-0 -z-20 h-full w-full overflow-hidden">
         <video
           src="https://res.cloudinary.com/dosqnvzhy/video/upload/v1779133379/hero_uwo0yz.mp4"
@@ -16,14 +17,19 @@ const Hero = () => {
           poster={heroIMG}
         ></video>
       </div>
-         {/* Overlay */}
-         <div className="absolute inset-0 -z-10 bg-linear-to-b from-transparent from-70% to-black"></div>
+      {/* Overlay */}
+      <div className="absolute inset-0 -z-10 bg-linear-to-b from-transparent from-70% to-black"></div>
 
-         {/* Logo */}
-         <div className="relative z-20 flex h-screen flex-col justify-end pb-20">
-            <img src={logo} alt="Restaura Logo" className="w-full p-4" />
-            <p className="p-4 text-lg tracking-tighter text-white">Paris</p>
-         </div>
+      {/* Logo */}
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.5 }}
+        className="relative z-20 flex h-screen flex-col justify-end pb-20"
+      >
+        <img src={logo} alt="Restaura Logo" className="w-full p-4" />
+        <p className="p-4 text-lg tracking-tighter text-white">Paris</p>
+      </motion.div>
     </section>
   );
 };
