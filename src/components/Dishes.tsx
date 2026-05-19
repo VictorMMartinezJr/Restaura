674f3bcecb2e19cf9511adfa84638a28f28e571e
Victor <victorm.martinezjr@outlook.com>
2026-05-18 22:04:45 -0400

    feat(ui): add animation to Dishes section

--- src/components/Dishes.tsx
+++ src/components/Dishes.tsx
@@ -1,5 +1,21 @@
 import { DISHES } from "../constants";
 import DishCard from "./DishCard";
+import {motion} from "framer-motion";
+
+const containerVariants = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const itemVariants = {
+  hidden: { opacity: 0, y: 20 },
+  show: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+};
 
 const Dishes = () => {
   return (
@@ -7,13 +23,18 @@ const Dishes = () => {
       <h2 className="mb-8 text-center text-3xl tracking-tighter lg:text-4xl">
         Our Dishes
       </h2>
-      <div className="grid grid-cols-1 gap-8 md:grid-cols-3 xl:grid-cols-5">
+      <motion.div 
+        initial="hidden"
+        whileInView="show"
+        variants={containerVariants}
+        viewport={{ once: true }}
+      className="grid grid-cols-1 gap-8 md:grid-cols-3 xl:grid-cols-5">
         {DISHES.map((dish, index) => (
-          <div key={index} className="flex flex-col items-center">
+          <motion.div variants={itemVariants} key={index} className="flex flex-col items-center">
             <DishCard key={index} dish={dish} />
-          </div>
+          </motion.div>
         ))}
-      </div>
+      </motion.div>
     </section>
   );
 };
