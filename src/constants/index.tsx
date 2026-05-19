70c3f75835e7a2492e2deed04a8a44fbbd92dd82
Victor <victorm.martinezjr@outlook.com>
2026-05-18 20:57:24 -0400

    create Review section

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -130,3 +130,17 @@ export const CUSINES: Cuisine[] = [
       "Delight in the art of Japanese culinary excellence, offering a fusion of classic and modern flavors.",
   },
 ];
+
+// Review content
+interface ReviewContent {
+  name: string;
+  profession: string;
+  content: string;
+}
+
+export const REVIEW: ReviewContent = {
+  name: "Xaviour Fernando",
+  profession: "Food Critic",
+  content:
+    "“As a seasoned food critic, my expectations are always high when stepping into a new dining establishment. Restaura, with its unassuming exterior and elegantly designed interior, promised a unique culinary experience from the moment I walked in. And I must say, it delivered beyond my expectations.”",
+};
