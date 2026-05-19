29c572ae9c42c6f146d396037daee84785ae6b75
Victor <victorm.martinezjr@outlook.com>
2026-05-18 20:28:55 -0400

    feat(ui): add Expertise component

---
+++ src/components/Enterprise.tsx
@@ -0,0 +1,43 @@
+import { CUSINES } from "../constants";
+
+const Enterprise = () => {
+  return (
+    <section id="expertise">
+        {/* Title */}
+      <h2 className="my-8 text-center text-3xl tracking-tighter lg:text-4xl">
+        Our Expertise
+      </h2>
+
+      <div className="container mx-auto px-4">
+        {CUSINES.map((cuisine, index) => (
+          <div
+            key={index}
+            className="flex items-center border-b-4 border-dotted border-neutral-700/40 py-2"
+          >
+            {/* Number */}
+            <div className="shrink-0 pr-8 text-2xl">{cuisine.number}</div>
+
+            {/* Image */}
+            <div className="w-1/3 shrink-0">
+              <img
+                src={cuisine.image}
+                alt={cuisine.title}
+                className="h-auto rounded-3xl"
+              />
+            </div>
+
+            {/* Text content */}
+            <div className="pl-8">
+              <h3 className="text-2xl uppercase tracking-tighter text-rose-300">
+                {cuisine.title}
+              </h3>
+              <p className="mt-4 text-lg tracking-tighter">{cuisine.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Enterprise;
