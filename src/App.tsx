1b1d95c0d714225f2f08597b03cf196311516cea
Victor <victorm.martinezjr@outlook.com>
2026-05-18 22:05:27 -0400

    feat(ui): add all components to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import "./App.css";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Dishes from "./components/Dishes";
+import Enterprise from "./components/Enterprise";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Mission from "./components/Mission";
+import Navbar from "./components/Navbar";
+import Review from "./components/Review";
 
 function App() {
   return (
-    <>
-      <h1 className="text-red-500">Hello World</h1>
-    </>
+    <main>
+      <Navbar />
+      <Hero />
+      <div className="bg-black">
+        <Dishes />
+        <About />
+        <Mission />
+        <Enterprise />
+        <Review />
+        <Contact />
+        <Footer />
+      </div>
+    </main>
   );
 }
 
